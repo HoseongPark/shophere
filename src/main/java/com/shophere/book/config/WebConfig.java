8f121d4c5afd004f937b73bfce94c7dddb698743
Lion_Park <ghtjd5689@gmail.com>
2020-06-02 15:06:22 +0900

    CORS 허용정책

--- src/main/java/com/shophere/book/config/WebConfig.java
+++ src/main/java/com/shophere/book/config/WebConfig.java
@@ -11,15 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @RequiredArgsConstructor
-@Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://219.250.20.28:8080")
-//                .allowedOrigins("http://127.0.0.1:8080")
-//                .allowedOrigins("http://localhost:8080");
-//    }
 }
