ceb2704cdc18f5f1766bce4d88dedfbd7caacbec
Lion_Park <ghtjd5689@gmail.com>
2020-06-10 16:46:26 +0900

    User 이름 업데이트 추가

--- src/main/java/com/shophere/book/api/dto/users/UserUpdateDto.java
+++ src/main/java/com/shophere/book/api/dto/users/UserUpdateDto.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public class UserUpdateDto {
     private String password;
+    private String name;
 
     public void changeSecurityPassword(String securityPassword) {
         this.password = securityPassword;
