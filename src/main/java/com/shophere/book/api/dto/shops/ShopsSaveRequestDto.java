3e53977fbdf52a240bf0fdc3fda684fb3c39ba23
hoseong <ghtjd5689@gmail.com>
2020-06-25 06:58:03 +0900

    Owner별 상점등록 확인하기.

--- src/main/java/com/shophere/book/api/dto/shops/ShopsSaveRequestDto.java
+++ src/main/java/com/shophere/book/api/dto/shops/ShopsSaveRequestDto.java
@@ -14,25 +14,5 @@ public class ShopsSaveRequestDto {
     private String author;
     private Integer price;
     private String category;
-
-    @Builder
-    public ShopsSaveRequestDto(String title, String overView, String content, String author, Integer price, String category) {
-        this.title = title;
-        this.overView = overView;
-        this.content = content;
-        this.author = author;
-        this.price = price;
-        this.category = category;
-    }
-
-    public Shops toEntity() {
-        return Shops.builder()
-                .title(title)
-                .overView(overView)
-                .content(content)
-                .author(author)
-                .price(price)
-                .category(category)
-                .build();
-    }
+    private String email;
 }
